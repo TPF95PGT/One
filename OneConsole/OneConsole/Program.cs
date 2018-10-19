b46cab5197819704da571900c5165cf019a85135
Patrick Fleury <tipat_59@hotmail.com>
2018-10-18 21:51:54 -0400

    Test

--- OneConsole/OneConsole/Program.cs
+++ OneConsole/OneConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace OneConsole
 {
@@ -6,7 +7,37 @@ namespace OneConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int max_value = 100000000;
+
+            DateTime start_time = DateTime.Now;
+
+            for (int i = 0; i < max_value; i++)
+            {
+                if ((i % 100000) == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            DateTime finish_time = DateTime.Now;
+
+            Console.WriteLine("Run time : " + Math.Round((finish_time - start_time).TotalSeconds), 2);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Press any key to exit.");
+
+            ConsoleKeyInfo keyPressed = Console.ReadKey();
+
+            if (!keyPressed.Equals(null))
+            {
+                Console.WriteLine("Exiting");
+                Thread.Sleep(1000);
+            }
+            else
+            {
+                Console.WriteLine("Oups");
+            }
         }
     }
 }
